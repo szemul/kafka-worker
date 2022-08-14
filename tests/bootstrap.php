792cbee25650e050e365690e711275553158e182
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2022-08-15 00:13:05 +1200

    Fix tests without the rdkafka extension

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,7 @@ declare(strict_types=1);
 if (!defined('RD_KAFKA_RESP_ERR__AUTHENTICATION')) {
     require __DIR__ . '/../vendor/kwn/php-rdkafka-stubs/stubs/constants.php';
 }
+
+if (!class_exists('RdKafka\Conf')) {
+    require __DIR__ . '/../vendor/kwn/php-rdkafka-stubs/stubs/RdKafka/Conf.php';
+}
